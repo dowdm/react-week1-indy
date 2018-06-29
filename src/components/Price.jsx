d8e94917e7552bc84f9a98e9b4e83dd5107490c5
matt dowdney <theofficialmatt@gmail.com>
2018-06-29 15:25:49 -0700

    minor ui changes and stylesheets added

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './price.css';
 
 function Price(props){
   return (
--- src/components/RemainingPints.jsx
+++ src/components/RemainingPints.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './remainingpints.css';
 
 function RemainingPints(props){
   return (
