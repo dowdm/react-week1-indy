029d04406d1a7e14546c37385ff6ffb50c879801
matt dowdney <theofficialmatt@gmail.com>
2018-06-29 09:23:59 -0700

    add content and styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './header.css';
 
 function Header(){
+  var headerStyles = {
+    fontFamily: 'courier new',
+    fontSize: '60px',
+    color: 'white',
+    textAlign: 'center',
+    textShadow: '2px 3px #ff8080'
+  }
   return (
-    <div>
-
+    <div className='header-wrapper' style={headerStyles}>
+      <h1>Welcome to Matt's Faßecke</h1>
     </div>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Welcome(){
   return (
     <div>
-
+      <ul className='top-bar-links'>
+          <Link to='/'><li>Home</li></Link>
+          <Link to='/keglist'><li>Keg List</li></Link>
+          <Link to='/newkegform'><li>Admin</li></Link>
+        </ul>
     </div>
   );
 }
