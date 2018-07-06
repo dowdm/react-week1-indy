d3c15fba5f1c9a8718d9f50961ef3e6510aee0ad
matt dowdney <theofficialmatt@gmail.com>
2018-07-06 08:52:45 -0700

    begin state refactor with app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,63 @@ import NewKegForm from './NewKegForm';
 import KegList from './KegList';
 import { Switch, Route } from 'react-router-dom';
 import './app.css';
+import { v4 } from 'uuid';
 
 
-function App(){
-  return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Switch>
-        <Route exact path = '/' component={Welcome}/>
-        <Route exact path = '/kegs/new' component={NewKegForm}/>
-        <Route exact path = '/kegs' component={KegList}/>
-        <Route component={Error404}/>
-      </Switch>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList:{},
+      selectedKeg: null
+    };
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+    this.handleKegSelection = this.handleKegSelection.bind(this);
+    this.handleBuyButtonClick = this.handleBuyButtonClick.bind(this);
+  }
 
-  );
+  handleAddButtonClick(newKeg) {
+    let newKegId = v4();
+    let newMasterTamList = Object.assign({}, this.state.masterKegList, {
+      [newKegId]: newKeg
+    });
+    this.setState({masterKegList: newMasterKegList});
+  }
+
+  handleKegSelection(kegId) {
+    this.setState({selectedKeg: kegId});
+  }
+
+  handleBuyButtonClick(kegId) {
+    let newMasterKegList = Object.assign({}, this.state.masterKegList);
+    if( newMasterKegList[kegId].remaining >= 1){
+    newMasterKegList[kegId].remaining = this.state.masterKegList[kegId].remaining + 30;
+    } else {
+      newMasterTamList[kegId].remaining = 0;
+    }
+    newMasterTamList[tamId].eating = true;
+    this.setState(() => {newMasterKegList;});
+  }
+
+
+  render(){
+    return (
+      <div className='app-wrapper'>
+        <Header/>
+        <Switch>
+          <Route exact path = '/' component={Welcome}/>
+          <Route exact path = '/kegs/new' component={NewKegForm}/>
+          <Route exact path = '/kegs'  render={()=>
+            <div>
+              <KegList kegList={this.state.masterKegList} onKegSelection={this.handleKegSelection} onAdd={this.handleAddButtonClick} selectedKeg={this.state.selectedKeg} />
+            </div>
+          }/>
+          <Route component={Error404}/>
+        </Switch>
+      </div>
+
+    );
+  }
 }
 
 export default App;
